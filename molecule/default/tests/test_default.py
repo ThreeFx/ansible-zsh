a8ea4275fc4997f7e0976248b38a4c2a930dbea6
Ben Fiedler <bfiedler@ethz.ch>
2019-10-15 11:05:35 +0200

    Improve test quality, prepare testing with two users
    Sadly, we will have to wait for
    https://github.com/threefx/ansible-zsh/issues/1 first.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,17 +7,10 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_zshrc(host):
-    fps = ['.zshrc', '.zshrc.d']
-    fs = map(lambda f: host.file('/root/' + f), fps)
-
-    for f in fs:
-        assert f.exists
-
-
-def test_zprofile(host):
-    fps = ['.zprofile', '.zprofile.d']
-    fs = map(lambda f: host.file('/root/' + f), fps)
+def test_files_exist(host):
+    fps = ['.zshrc', '.zshrc.d', '.zprofile', '.zprofile.d']
+    homes = ['/root']  # , '/home/testuser']
+    fs = map(lambda f: host.file(f), [h + '/' + f for h in homes for f in fps])
 
     for f in fs:
         assert f.exists
